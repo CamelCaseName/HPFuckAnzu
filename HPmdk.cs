da238681ceffe9fb12fe46acce8caf0626f9b335
Leonhard Seidel <leonhardseidel@gmx.de>
2021-07-26 21:57:15 +0200

    Add project files.

---
+++ HPmdk.cs
@@ -0,0 +1,23 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project
+{
+    public class HPFuckAnzu : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            GameObject[] objects = scene.GetRootGameObjects();
+            foreach (GameObject item in objects)
+            {
+                if (item.name == "AnzuAds")
+                {
+                    MelonLogger.Msg("Deactivated AnzuAds!");
+                    item.SetActive(false);
+                }
+            }
+        }
+    }
+}
